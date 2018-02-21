630cf0b4bd4fc88dc83263b000afac362c588cad
Israel Diego Lalo <ilalo@ABCNBK98.abcconsulting.com.ar>
2018-02-21 13:16:44 +0000

    Empty spring boot project

---
+++ src/main/java/com/dlalo/solsticebackend/SolsticeBackendApplication.java
@@ -0,0 +1,12 @@
+package com.dlalo.solsticebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolsticeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolsticeBackendApplication.class, args);
+	}
+}
